aa860574c84047a1d66197799e94144ffa9327fc
nora <sxj042@gmail.com>
2023-04-19 13:42:52 -0400

    working

---
+++ src/main/java/daos/ConnectionFactory.java
@@ -0,0 +1,34 @@
+package daos;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    
+
+    public static final String URL = "jdbc:mysql://localhost:3306/cars";
+    public static final String USER = "nora";
+    public static final String PASS = "MeowMeow";
+    
+    /**
+     * Get a connection to database
+     * @return Connection object
+    */
+    public static Connection getConnection() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error connecting to the database", ex);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Connection connection = ConnectionFactory.getConnection();
+    }
+
+}
+
